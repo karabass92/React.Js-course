1724a74d54be74fc7752ca7f097d8ab54af91fca
karabass92 <shatveryank@gmail.com>
2023-01-24 22:06:50 +0600

    dialog page layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,19 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>Person 1</div>
+                <div className={classes.dialog}>Person 2</div>
+                <div className={classes.dialog}>Person 3</div>
+                <div className={classes.dialog}>Person 4</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>What's you doing?</div>
+                <div className={classes.message}>Fine</div>
+            </div>
         </div>
     );
 }
