313ef3c7d8cbd27917aec44c3ea989b78646b471
karabass92 <shatveryank@gmail.com>
2023-01-29 00:40:12 +0600

    style_NAV

--- src/components/Navbar/FriendsBlock/FriendsBlock.jsx
+++ src/components/Navbar/FriendsBlock/FriendsBlock.jsx
@@ -3,11 +3,31 @@ import classes from './FriendsBlock.module.css'
 
 
 const FriendsBlock = (props) => {
+
+    let sideBar = {
+        friendsData: [
+            { id: 1, name: 'Person 1' },
+            { id: 2, name: 'Person 2' },
+            { id: 3, name: 'Person 3' },
+        ],
+    };
+    
     return (
         <div className={classes.friendsBlock}>
             <span>Friends</span>
-            <div>
-                
+            <div className={classes.friendItems}>
+                <div className={classes.friendItem}>
+                    <img src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
+                    <span>{sideBar.friendsData[0].name}</span>
+                </div>
+                <div className={classes.friendItem}>
+                    <img src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
+                    <span>{sideBar.friendsData[1].name}</span>
+                </div>                
+                <div className={classes.friendItem}>
+                    <img src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
+                    <span>{sideBar.friendsData[2].name}</span>
+                </div>
             </div>
         </div>
     );
