8d27e8b54960ca9e6eda1dd1bc17e747f52786c8
karabass92 <shatveryank@gmail.com>
2023-01-30 21:29:24 +0600

    none

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = (props) => {
     return (
-        <div>
+        <div className={classes.bb}>
             <ProfileInfo />
             <MyPosts postsData={props.postsData} addPost={props.addPost} />
         </div>
