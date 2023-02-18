802e9d4827ede174eb13d35255c5522aaa84f9c5
karabass92 <shatveryank@gmail.com>
2023-02-19 00:16:49 +0600

    withRouter

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
                 <Navbar />
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path='/profile/*' element={<ProfileContainer />} />
+                        <Route path='/profile/:userId?' element={<ProfileContainer />} />
                         <Route path='/dialogs/*' element={<DialogsContainer />} />
                         <Route path='/news' element={<News />} />
                         <Route path='/music' element={<Music />} />
